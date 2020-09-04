165e79a0d5ed97189e86dde9963a8e38c3d37330
Felix J Lee <ka.felix496@gmail.com>
2020-09-04 16:59:42 -0400

    prepare to publish, build publish files.

--- npm/umd/customLibrary.js
+++ npm/umd/customLibrary.js
@@ -1,20 +1,247 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('react')) :
-  typeof define === 'function' && define.amd ? define(['react'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.customLibrary = factory(global.React));
-}(this, (function (React) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react'), require('prop-types')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'react', 'prop-types'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.customLibrary = {}, global.React, global.PropTypes));
+}(this, (function (exports, React, PropTypes) { 'use strict';
 
   function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
   var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+  var PropTypes__default = /*#__PURE__*/_interopDefaultLegacy(PropTypes);
 
-  var testCss = ".test {\n  display: none;\n  background-color: black;\n}\n";
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
 
-  var Test = function Test() {
-    var testValue = 'test';
-    return /*#__PURE__*/React__default['default'].createElement(React__default['default'].Fragment, null, /*#__PURE__*/React__default['default'].createElement("style", null, testCss), /*#__PURE__*/React__default['default'].createElement("div", null, testValue));
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
+
+  function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function");
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) _setPrototypeOf(subClass, superClass);
+  }
+
+  function _getPrototypeOf(o) {
+    _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+      return o.__proto__ || Object.getPrototypeOf(o);
+    };
+    return _getPrototypeOf(o);
+  }
+
+  function _setPrototypeOf(o, p) {
+    _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+      o.__proto__ = p;
+      return o;
+    };
+
+    return _setPrototypeOf(o, p);
+  }
+
+  function _isNativeReflectConstruct() {
+    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+    if (Reflect.construct.sham) return false;
+    if (typeof Proxy === "function") return true;
+
+    try {
+      Date.prototype.toString.call(Reflect.construct(Date, [], function () {}));
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function _assertThisInitialized(self) {
+    if (self === void 0) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+
+    return self;
+  }
+
+  function _possibleConstructorReturn(self, call) {
+    if (call && (typeof call === "object" || typeof call === "function")) {
+      return call;
+    }
+
+    return _assertThisInitialized(self);
+  }
+
+  function _createSuper(Derived) {
+    var hasNativeReflectConstruct = _isNativeReflectConstruct();
+
+    return function _createSuperInternal() {
+      var Super = _getPrototypeOf(Derived),
+          result;
+
+      if (hasNativeReflectConstruct) {
+        var NewTarget = _getPrototypeOf(this).constructor;
+
+        result = Reflect.construct(Super, arguments, NewTarget);
+      } else {
+        result = Super.apply(this, arguments);
+      }
+
+      return _possibleConstructorReturn(this, result);
+    };
+  }
+
+  function _slicedToArray(arr, i) {
+    return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
+  }
+
+  function _arrayWithHoles(arr) {
+    if (Array.isArray(arr)) return arr;
+  }
+
+  function _iterableToArrayLimit(arr, i) {
+    if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _e = undefined;
+
+    try {
+      for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+        _arr.push(_s.value);
+
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"] != null) _i["return"]();
+      } finally {
+        if (_d) throw _e;
+      }
+    }
+
+    return _arr;
+  }
+
+  function _unsupportedIterableToArray(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(o);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+  }
+
+  function _arrayLikeToArray(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+
+    for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+    return arr2;
+  }
+
+  function _nonIterableRest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+  }
+
+  var RangeCss = "input {\n  background-color: black;\n}\n";
+
+  var Range = function Range(props) {
+    var min = props.min,
+        max = props.max,
+        value = props.value;
+
+    var _React$useState = React__default['default'].useState(value),
+        _React$useState2 = _slicedToArray(_React$useState, 2),
+        currentValue = _React$useState2[0],
+        setValue = _React$useState2[1];
+
+    var onChange = function onChange(event) {
+      return setValue(event.target.value);
+    };
+
+    return /*#__PURE__*/React__default['default'].createElement(React__default['default'].Fragment, null, /*#__PURE__*/React__default['default'].createElement("style", null, "".concat(RangeCss)), /*#__PURE__*/React__default['default'].createElement("input", {
+      type: "range",
+      "data-testid": "range-test",
+      min: min,
+      max: max,
+      value: currentValue,
+      onChange: onChange
+    }));
   };
 
-  return Test;
+  Range.propTypes = {
+    min: PropTypes__default['default'].number.isRequired,
+    max: PropTypes__default['default'].number.isRequired,
+    value: PropTypes__default['default'].number.isRequired
+  };
+
+  var CheckboxWithLabel = /*#__PURE__*/function (_React$Component) {
+    _inherits(CheckboxWithLabel, _React$Component);
+
+    var _super = _createSuper(CheckboxWithLabel);
+
+    function CheckboxWithLabel(props) {
+      var _this;
+
+      _classCallCheck(this, CheckboxWithLabel);
+
+      _this = _super.call(this, props);
+      _this.state = {
+        isChecked: false
+      }; // bind manually because React class components don't auto-bind
+      // https://reactjs.org/blog/2015/01/27/react-v0.13.0-beta-1.html#autobinding
+
+      _this.onChange = _this.onChange.bind(_assertThisInitialized(_this));
+      return _this;
+    }
+
+    _createClass(CheckboxWithLabel, [{
+      key: "onChange",
+      value: function onChange() {
+        this.setState({
+          isChecked: !this.state.isChecked
+        });
+      }
+    }, {
+      key: "render",
+      value: function render() {
+        return /*#__PURE__*/React__default['default'].createElement("label", null, /*#__PURE__*/React__default['default'].createElement("input", {
+          type: "checkbox",
+          checked: this.state.isChecked,
+          onChange: this.onChange
+        }), this.state.isChecked ? this.props.labelOn : this.props.labelOff);
+      }
+    }]);
+
+    return CheckboxWithLabel;
+  }(React__default['default'].Component);
+
+  exports.Checkbox = CheckboxWithLabel;
+  exports.Range = Range;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
