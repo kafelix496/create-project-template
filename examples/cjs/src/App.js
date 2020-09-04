00d710b1a6220ad1ff5502ff3213635f5fd40832
Felix J Lee <ka.felix496@gmail.com>
2020-09-04 16:58:07 -0400

    make examples

---
+++ examples/cjs/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Range } from '@kafelix496/react-library-template'
+
+function App() {
+  return (
+    <Range min={0} max={10} value={5} />
+  );
+}
+
+export default App;
